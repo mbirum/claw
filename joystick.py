e4fa534af23512f549a2a07d31a06d43331dc14c
Matt Birum <45514707+mbirum@users.noreply.github.com>
2025-11-12 11:35:39 -0500

    Update joystick.py

--- joystick.py
+++ joystick.py
@@ -1,5 +1,28 @@
 import pygame
 import os
+import RPi.GPIO as GPIO
+import time
+import motor_seq
+import sys
+import subprocess
+import select
+
+GPIO.setmode(GPIO.BOARD)
+
+left_pins = [7,11,13,15]
+right_pins = [16,18,22,32]
+sleep = 0.001
+
+control_pins = right_pins
+sequence = motor_seq.getForwardSequence()
+
+# max 512
+rotation = 20
+
+# initialize pins
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
 
 pygame.init()
 pygame.joystick.init()
@@ -27,7 +50,6 @@ else:
 					print(f"right {event.value}")
 			elif event.type == pygame.JOYAXISMOTION and event.axis == 1:
 				if event.value < 0:
-					# print(f"up {event.value}")
-					os.system("~/pymotor/env/bin/python3 ~/pymotor/motor.py right &")
+					print(f"up {event.value}")
 				elif event.value > 0:
 					print(f"down {event.value}")
